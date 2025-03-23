872665a3a99450a46ebb9ee52e198a0420a41e88
sitmsiteman <sitmsiteman@gmail.com>
2025-03-23 17:24:58 +0900

    fix2-test

--- quack.el
+++ quack.el
@@ -3123,7 +3123,9 @@ Can be used in your `~/.emacs' file something like this:
 
           ((eq quack-switch-to-scheme-method 'own-frame)
            (quack-force-frame-switch-to-window
-            (display-buffer-pop-up-frame repl-buf)))
+            (display-buffer-pop-up-frame repl-buf (push pop-up-windows
+							'(pop-up-windows t)
+							display-buffer-alist))))
 
           (t (error "Invalid quack-switch-to-scheme-method: %S"
                     quack-switch-to-scheme-method)))))
