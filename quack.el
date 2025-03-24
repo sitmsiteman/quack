c8eab710ced9e309cb0900b85d90c0636fccb073
sitmsiteman <sitmsiteman@gmail.com>
2025-03-25 04:09:29 +0900

    quack-global-menu location fix

--- quack.el
+++ quack.el
@@ -3485,10 +3485,8 @@ Can be used in your `~/.emacs' file something like this:
      ;;   (setq menu-bar-final-items (cons 'Quack menu-bar-final-items)))
      (easy-menu-define quack-global-menu global-map "Quack"
        quack-global-menuspec)
-     ;; (define-key-after global-map [menu-bar quack]
-     ;; 		 (cons "Quack"
-     ;; 		       (or (lookup-key map [menu-bar Quack])
-     ;; 			   (lookup-key map [menu-bar quack]))))
+     (define-key-after global-map [menu-bar quack]
+		 (cons "Quack" (make-sparse-keymap "Quack")) 'Tools)
      )
     (quack-when-xemacs
      ;; Die! Die! Die!
