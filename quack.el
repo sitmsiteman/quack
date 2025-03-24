ef27635fa87f10a47fb6ed9a3c8b969c79af9b16
sitmsiteman <sitmsiteman@gmail.com>
2025-03-25 05:00:48 +0900

    replace assq

--- quack.el
+++ quack.el
@@ -3487,7 +3487,7 @@ Can be used in your `~/.emacs' file something like this:
 (defun quack-install-global-menu ()
   (when quack-global-menu-p
     (quack-when-gnuemacs
-     (unless (assq 'Quack menu-bar-final-items)
+     (unless (cl-position 'Quack menu-bar-final-items)
        (push 'Quack menu-bar-final-items))
      (easy-menu-define quack-global-menu global-map ""
        quack-global-menuspec))
